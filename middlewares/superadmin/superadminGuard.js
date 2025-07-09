832a8852c7d49238593c1e35016be0072e171126
Peter Manoukian <thewizard_it@hotmail.com>
2025-07-09 12:09:35 +0400

    step 19

--- middlewares/superadmin/superadminGuard.js
+++ middlewares/superadmin/superadminGuard.js
@@ -1,6 +1,7 @@
 //middlewares\superadmin\superadminGuard.js
 const superadminGuard = (req, res, next) => {
   const user = req.session.user;
+  
 
   if (!user || user.roler !== 'superadmin') {
     return res.status(403).send('Access denied from superamdin middleware Line 6');
